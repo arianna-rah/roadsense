334cf0a68f9fea6db1022616231d132fabfa5044
Arianna R <arirah09@gmail.com>
2026-01-16 14:51:41 -0500

    integrated yolo model logic & connected to front-end, added pre-processing before cnn usage, changed ui & added advice to user based on model output

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,11 @@ from PIL import Image
 from torchvision import transforms
 import torch
 import timm
+import ultralytics
 import io
+import base64
 from fastapi.middleware.cors import CORSMiddleware
+from ultralytics import YOLO
 
 app = FastAPI()
 
@@ -28,7 +31,7 @@ img_transforms = transforms.Compose([
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
 
-class_to_idx = {0: "dry", 1: "wet", 2: "standing_water", 3: "snow", 4: "ice"}
+class_to_idx = {0: "dry", 1: "wet", 2: "standing water", 3: "snow", 4: "ice"}
 
 model = timm.create_model('tf_mobilenetv3_large_100.in1k', pretrained=False)
 model.classifier = torch.nn.Sequential(
@@ -44,6 +47,8 @@ model.load_state_dict(state_dict)
 model.eval()
 model.to(device)
 
+yolo_model = YOLO('best (2).pt')
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     if not file.content_type.startswith('image/'):
@@ -51,9 +56,12 @@ async def predict(file: UploadFile = File(...)):
     try: 
         contents = await file.read()
         img = Image.open(io.BytesIO(contents)).convert('RGB')
-        img = img_transforms(img).unsqueeze(0).to(device)
+        w, h = img.size
+        crop_box = (w/3, h/2, 2*w/3, h)
+        cropped_img = img.crop(crop_box)
+        cropped_img = img_transforms(cropped_img).unsqueeze(0).to(device)
         with torch.no_grad():
-            label_pred = model(img)
+            label_pred = model(cropped_img)
             probs = torch.nn.functional.softmax(label_pred, dim=1)
             confidence, predicted = torch.max(probs, 1)
     
@@ -70,5 +78,31 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post('/yolo-predict')
+async def yolo_predict(file: UploadFile = File(...)):
+    if not file.content_type.startswith('image/'):
+        raise HTTPException(status_code=400, detail="File must be an image")
+    try:
+        contents = await file.read()
+        img = Image.open(io.BytesIO(contents)) 
+        results = yolo_model.predict(
+        source=img,
+        save=False,          
+        conf=0.05,
+        device = 0           
+        )
+        num_anomalies = len(results[0].boxes)
+        result_img = Image.fromarray(results[0].plot())
+        buffered = io.BytesIO()
+        result_img.save(buffered, format="JPEG")
 
+        img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+        result_url = f"data:image/jpeg;base64,{img_base64}"
+        return {
+            "num_anomalies": num_anomalies,
+            "result_img": result_url
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
--- roadsense/app/components/uploadfile.tsx
+++ roadsense/app/components/uploadfile.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef } from 'react';
-import { Upload } from 'lucide-react';
+import { Upload, RefreshCcw } from 'lucide-react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
@@ -8,8 +8,9 @@ import Image from 'next/image';
 export function UploadFile() {
     const [isDragging, setIsDragging] = useState(false);
     const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-    const [fileSelect, setFileSelect] = useState<File | null>(null);
+    // const [fileSelect, setFileSelect] = useState<File | null>(null);
     const [resultA, setResultA] = useState("Result");
+    const [resultB, setResultB] = useState<string | null>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const handleDragOver = (e: React.DragEvent) => {
@@ -42,17 +43,42 @@ export function UploadFile() {
         if (file.type.startsWith('image/')) {
             const previewUrl = URL.createObjectURL(file);
             setPreviewUrl(URL.createObjectURL(file));
-            setFileSelect(file);
+            // setFileSelect(file);
             const formData = new FormData();
             formData.append('file', file);
             try {
-                const response = await fetch("http://localhost:8000/predict", {
+                const responseB = await fetch("http://localhost:8000/yolo-predict", {
                     method: "POST",
                     body: formData
                 });
-                const data = await response.json();
-                console.log(data);
-                setResultA(`There is likely ${data.predicted} on the road. Be careful!!`);
+                const dataB = await responseB.json();
+                setResultB(dataB.result_img);
+                const responseA = await fetch("http://localhost:8000/predict", {
+                    method: "POST",
+                    body: formData
+                });
+                const dataA = await responseA.json();
+                if (dataA.predicted == "dry" && dataB.num_anomalies <= 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Maintain normal following distance and smooth steering, braking, and acceleration.`);
+                } else if (dataA.predicted == "dry" && dataB.num_anomalies > 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Maintain normal following distance and smooth steering, braking, and acceleration. Stay alert for sudden hazards like debris or animals, especially at higher speeds. Slow down slightly and watch for potholes, cracks, or uneven pavement that could affect steering or tires!`);
+                } else if (dataA.predicted == "wet" && dataB.num_anomalies <= 1) {
+                    setResultA(`The road is likely ${dataA.predicted}, and it has ${dataB.num_anomalies} damages on it. Reduce speed and increase following distance, as traction and braking efficiency are reduced. Avoid sudden movements and be cautious of hydroplaning, especially just after rain begins.`)
+                } else if (dataA.predicted == "wet" && dataB.num_anomalies > 1) {
+                    setResultA(`The road is likely ${dataA.predicted}, and it has ${dataB.num_anomalies} damages on it. Reduce speed and increase following distance, as traction and braking efficiency are reduced. Avoid sudden movements and be cautious of hydroplaning, especially just after rain begins. Watch for potholes, cracks, or uneven pavement that could affect steering or tires!`)
+                } else if (dataA.predicted == "standing water" && dataB.num_anomalies <= 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Slow down significantly and avoid driving through deep water if possible, as it can cause loss of control or engine damage. If unavoidable, drive steadily without braking hard and test brakes afterward.`)
+                } else if (dataA.predicted == "standing water" && dataB.num_anomalies > 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Slow down significantly and avoid driving through deep water if possible, as it can cause loss of control or engine damage. If unavoidable, drive steadily without braking hard and test brakes afterward. Watch for potholes, cracks, or uneven pavement that could affect steering or tires!`)
+                } else if (dataA.predicted == "snow" && dataB.num_anomalies <= 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Drive slowly, accelerate gently, and brake early to prevent skidding. Increase following distance and use lower gears when descending hills for better control.`)
+                } else if (dataA.predicted == "snow" && dataB.num_anomalies > 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Drive slowly, accelerate gently, and brake early to prevent skidding. Increase following distance and use lower gears when descending hills for better control. Watch for potholes, cracks, or uneven pavement that could affect steering or tires!`)
+                } else if (dataA.predicted == "ice" && dataB.num_anomalies <= 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it.  Assume very little traction and drive at extremely reduced speeds. Avoid sudden steering, braking, or acceleration, and increase following distance dramatically.`)
+                } else if (dataA.predicted == "ice" && dataB.num_anomalies > 1) {
+                    setResultA(`There is likely ${dataA.predicted} on the road, and the road has ${dataB.num_anomalies} damages on it. Assume very little traction and drive at extremely reduced speeds. Avoid sudden steering, braking, or acceleration, and increase following distance dramatically. Watch for potholes, cracks, or uneven pavement that could affect steering or tires!`)
+                }
             } catch (e) {
                 console.error("Error", e)
             }
@@ -93,13 +119,34 @@ export function UploadFile() {
                             className="relative rounded-lg object-cover"/>
                     </div>)}
                 </label>
-                <div className="border-2 border-dashed border-slate-700 bg-slate-800/50 rounded-lg min-h-100 p-10">
-                    <h2 className="text-white text-2xl">{resultA}</h2>
-                </div>
+                {!resultB
+                    ? (<motion.div 
+                        onDragOver={handleDragOver}
+                        onDragLeave={handleDragLeave}
+                        whileHover={{ scale: 1.02 }}
+                        className={`flex flex-col border-2 border-dashed border-gray-500 rounded-lg p-8 min-h-100 items-center justify-center transition-all cursor-pointer ${
+                            isDragging
+                                ? 'border-yellow-400 bg-yellow-400/10'
+                                : 'border-slate-700 bg-slate-800/50 hover:border-slate-600 hover:bg-slate-800'
+                        }`}
+                        >
+                            <div className="bg-slate-700 w-24 h-24 rounded-full p-6 mb-6">
+                                <RefreshCcw className="w-12 h-12 text-slate-400 mb-4"/>
+                            </div>
+                            <p className="text-white text-xl font-bold">Loading damage results here...</p>
+                            <p className="text-slate-500 text-lg">Give it a second!</p>
+                        </motion.div>)
+                    : (<div className="relative min-h-100 w-full">
+                        <Image 
+                            src={resultB}
+                            alt='Road Image'
+                            fill
+                            className="relative rounded-lg object-cover"/>
+                    </div>)}
             </div>
             <div className="grid grid-cols-1 max-w-6xl mx-auto mt-10">
                 <div className="border-2 border-dashed border-slate-700 bg-slate-800/50 rounded-lg min-h-64 p-10">
-                    <h2 className="text-white text-5xl">Results</h2>
+                    <h2 className="text-white text-3xl">{resultA}</h2>
                 </div>
             </div>
         </div>
